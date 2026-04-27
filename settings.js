1517439ab8caa165f16101b0125bac6ea825701b
gaveetoon18-pixel <gaveetoon18@gmail.com>
2026-04-27 18:46:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,9 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=fee04a0ee26e804b' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=0ef3fd9c5295d7d8' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
-
-
